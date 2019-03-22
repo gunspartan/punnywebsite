6662543d70b4f2e1aabb796fc0e2069b65ffa154
Suna <gunhwang99@gmail.com>
2019-03-22 16:00:28 -0400

    new pun button working, submitting pun still non-functional error 405, method not allowed

--- pun.js
+++ pun.js
@@ -1,7 +1,53 @@
-var puns = ["pun1", "pun2", "pun3", "pun4"];
-var randomNum = Math.floor(Math.random() * (puns.length - 1));
-var randomPun = puns[randomNum];
-
 $(function() {
-  $("#pun").html(randomPun);
+  var $pun = $("#pun");
+  var $name = $("#name");
+  var $content = $("#content");
+
+  function loadPun(puns) {
+    var randomNum = Math.floor(Math.random() * puns.length);
+    var randomPun = puns[randomNum];
+    $pun.html(randomPun.punTitle + "<br> <p>" + randomPun.punContent + "</p>");
+  }
+  $.ajax({
+    type: "GET",
+    url: "/api/puns.json",
+    success: function(puns) {
+      loadPun(puns);
+    },
+    error: function() {
+      alert("error loading puns");
+    }
+  });
+
+  $("#new-pun").on("click", function() {
+    $.ajax({
+      type: "GET",
+      url: "/api/puns.json",
+      success: function(newPun) {
+        loadPun(newPun);
+      },
+      error: function() {
+        alert("error loading puns");
+      }
+    });
+  });
+
+  $("#add-pun").on("click", function() {
+    var punToPost = {
+      punTitle: $name.val(),
+      punContent: $content.val()
+    };
+
+    $.ajax({
+      type: "GET",
+      url: "/api/puns.json",
+      data: punToPost,
+      success: function() {
+        alert("Pun added");
+      },
+      error: function() {
+        alert("error saving pun");
+      }
+    });
+  });
 });
